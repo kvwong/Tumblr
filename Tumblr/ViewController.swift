496ed14854c7ffb4b5374a2523a10a84968b137a
Kevin Wong <kdubz313@gmail.com>
2015-10-10 13:01:05 -0700

    Initial Commit

---
+++ Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr
+//
+//  Created by Kevin Wong on 10/10/15.
+//  Copyright © 2015 Kevin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
